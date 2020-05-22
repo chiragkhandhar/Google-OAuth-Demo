85b1a2b7c3b6d46e6a6ccc2dcc8c5af5fa53307b
Chirag Khandhar <ckhandhar@hawk.iit.edu>
2020-05-22 12:26:38 -0500

    Google SignIn Done

--- app/src/main/java/ml/chiragkhandhar/ecospot/MainActivity.java
+++ app/src/main/java/ml/chiragkhandhar/ecospot/MainActivity.java
@@ -1,15 +1,165 @@
 package ml.chiragkhandhar.ecospot;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.bumptech.glide.Glide;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+public class MainActivity extends AppCompatActivity 
+{
+    private static final String TAG = "MainActivity";
+    SignInButton signInButton;
+    Button signOut;
+    int RC_SIGN_IN = 0;
+
+    TextView name, email;
+    ImageView dp;
+
+    GoogleSignInOptions gso;
+    GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setUpComponents();
+
+
+
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+
+        signInButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                signIn();
+            }
+        });
+    }
+
+    private void signIn()
+    {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    private void signOut()
+    {
+        mGoogleSignInClient.signOut()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task)
+                    {
+                        name.setVisibility(View.INVISIBLE);
+                        email.setVisibility(View.INVISIBLE);
+                        dp.setVisibility(View.INVISIBLE);
+                        signInButton.setVisibility(View.VISIBLE);
+                        signOut.setVisibility(View.INVISIBLE);
+                        Toast.makeText(MainActivity.this, "Sign Out Successfull", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void setUpComponents()
+    {
+        signInButton = findViewById(R.id.sign_in_button);
+        signInButton.setSize(SignInButton.SIZE_STANDARD);
+        name = findViewById(R.id.name);
+        email = findViewById(R.id.email);
+        dp = findViewById(R.id.dp);
+        signOut = findViewById(R.id.signout);
+
+        signOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signOut();
+
+            }
+        });
+    }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        updateUI(account);
+    }
+
+    private void updateUI(GoogleSignInAccount account) 
+    {
+        if(account != null)
+        {
+            name.setVisibility(View.VISIBLE);
+            email.setVisibility(View.VISIBLE);
+            dp.setVisibility(View.VISIBLE);
+            signInButton.setVisibility(View.INVISIBLE);
+            signOut.setVisibility(View.VISIBLE);
+
+            name.setText(account.getDisplayName());
+            email.setText(account.getEmail());
+            Glide.with(this).load(account.getPhotoUrl().toString()).into(dp);
+            Log.d(TAG, "updateUI: bp: Not signed in yet");
+        }
+        else
+        {
+            Log.d(TAG, "updateUI: bp: Already signed in");
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask)
+    {
+        try
+        {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            // Signed in successfully, show authenticated UI.
+            updateUI(account);
+        }
+        catch (ApiException e)
+        {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(TAG, "bp: signInResult:failed code=" + e.getStatusCode());
+            updateUI(null);
+        }
     }
 }
